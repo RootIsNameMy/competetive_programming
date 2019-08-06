2758e90d999a8f8d00b18ef0e1fa2f1816442312
Tilen Koren <koren.aka.root@gmail.com>
2019-08-06 22:12:19 +0200

    Automatic commit 08/06/2019

--- push.sh
+++ push.sh
@@ -9,7 +9,8 @@ date +'%r'
 
 ## backup dir format ##
 backup_dir=$(date +'%m/%d/%Y')
-echo "Backup dir for today: /nas04/backups/"
 
 git add .
 git commit -m "Automatic commit ${backup_dir}"
+
+git push https://github.com/RootIsNameMy/competetive_programming.git master
