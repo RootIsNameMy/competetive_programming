a255811770ecae5ef773a16fa70e52ac3a1c6e1f
Tilen Koren <koren.aka.root@gmail.com>
2019-08-06 22:17:28 +0200

    Automatic commit 08/06/2019

--- push.sh
+++ push.sh
@@ -13,4 +13,4 @@ backup_dir=$(date +'%m/%d/%Y')
 git add .
 git commit -m "Automatic commit ${backup_dir}"
 
-git push https://RootIsNameMy:Ih4v3R00ts@competetive_programming.biz master
+git push git@github.com:RootIsNameMy/competetive_programming.git master
